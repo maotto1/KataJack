6a5ec8bace63fd00d940e8d3a0936f0f836580e7
otto@flowdesk <alexander@flowdesk.de>
2022-07-25 23:53:46 +0200

    inheritance solution for game ending

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -2,6 +2,7 @@ fun main(args: Array<String>) {
     val bowlingGame = BowlingGame()
 
     val rolls = listOf(1, 4, 4, 5, 6, 4, 5, 5, 10, 0, 1, 7, 3, 6, 4, 10, 2, 8, 6)
+    val testData = listOf(4,2,8,2,6,3,10,3,7,0,8,10,10,7,0,5,5,1)
 
     rolls.map { pins ->
         {
